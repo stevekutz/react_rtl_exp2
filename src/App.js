617946e3be6b616286806260ccb9711bfba86bb6
stevekutz <stkutz@gmail.com>
2019-07-12 15:25:42 -0500

    added getByLabelText implmentation

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ class App extends React.Component {
   render() {
     return (
       <section aria-labelledby = "KittensHeader">
+        <label htmlFor="username">Username</label>
+        <input id="username" />
         <h2 id = "KitttensHeader">All about Kittens</h2>
         <p className = "content"> Lorem ipsum dolor sit amet</p>
       </section>
