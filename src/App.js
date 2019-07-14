d47fd1364a04d8279be430afdfbc38d6c0c7a570
stevekutz <stkutz@gmail.com>
2019-07-13 23:32:30 -0500

    added form and implementation of getByLabelText

--- src/App.js
+++ src/App.js
@@ -4,12 +4,40 @@ import React from 'react';
 class App extends React.Component {
   render() {
     return (
-      <section aria-labelledby = "KittensHeader">
-        <label htmlFor="username">Username</label>
-        <input id="username" alt="userInfo"/>
-        <h2 id = "KitttensHeader">All about Kittens</h2>
-        <p className = "content"> Lorem ipsum dolor sit amet</p>
-      </section>
+      <div>
+          <section aria-labelledby = "KittensHeader">
+            <label htmlFor="username">Username</label>
+            <input id="username" alt="userInfo"/>
+            <h2 id = "KitttensHeader">All about Kittens</h2>
+            <p className = "content"> React elements use htmlFor</p>
+          </section>
+
+          <div>
+            
+          <form action="" method="get" className="formExp">
+              <div className="formName">
+                <label htmlFor="fullName">Enter your name: </label>
+                <input type="text" name="fullName" id="fullName" required/>
+              </div>
+
+              <div className="formEmail">
+                <label htmlFor="email">Enter your email: </label>
+                <input type="email" name="email" id="email" required/>
+              </div>
+
+              <div className="formInput">
+                <input type="submit" value="Login"/>
+              </div>
+        </form>
+          
+          
+          
+          
+          </div>
+
+
+      </div>
+
     );
   }
 }
